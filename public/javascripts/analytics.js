f8ef74d6289bf0e8248160ab9e446fd46dfa1e5a
pacochavez <fco_chavezg@hotmail.com>
2014-12-01 20:01:17 -0700

    google dev

--- public/javascripts/analytics.js
+++ public/javascripts/analytics.js
@@ -3,4 +3,5 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 ga('create', 'UA-11728587-17', 'auto');
+ga('require', 'displayfeatures');
 ga('send', 'pageview');
